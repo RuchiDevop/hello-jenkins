e43bc171f30e6a170b5d8103b5ededab79bdec28
RuchiDevop <RuchiDevop@users.noreply.github.com>
2015-03-09 10:45:46 +0530

    Checking automatic build when change added here

--- Test.pl
+++ Test.pl
@@ -2,3 +2,4 @@ print "testing automatic build finally\n";
 print "using ngork with localhost port forwarding mechanism for local dev enviroment!!!"
 print "added repository to the jenkins and applied the settings to jenkins"
 print "ngork url everywhere instead of local host"
+print "\nChecking automatic build when change added here\n"
